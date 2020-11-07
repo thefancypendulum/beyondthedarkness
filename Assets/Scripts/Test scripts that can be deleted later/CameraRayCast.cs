77db1b039059c7a3ef8d3fcd50c170c169227ec5
Lachlan <mindcraftlachlan@outlook.com>
2020-11-07 22:27:35 +1100

    functional buttons and a few assets
    added fully funcitonal world space buttons as well as a few sprites and textures to start making a mock-up main menu. I just need to pull to make sure i dont override any existing folders that trep might have made (models)

---
+++ Assets/Scripts/Test scripts that can be deleted later/CameraRayCast.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRayCast : MonoBehaviour
+{
+    public bool MenuRaycast = true;
+
+    //stored infomation for the raycast hit
+    GameObject HitObject;
+
+    GameObject PreviousHitObject;
+
+    
+
+    enum Layers
+    {
+        //just to make the if statements more readable and idiot proof
+        UI = 5
+    }
+
+    void Update()
+    {
+        
+        if (MenuRaycast) //only raycast if the bool is true
+        {
+            //raycast
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 30))
+            {
+                //store some information about the raycast hit
+                HitObject = hit.transform.gameObject;
+            }
+        }
+
+        
+
+
+        if (HitObject != null) //put all hit obj logic inside here
+        {
+            if (PreviousHitObject != HitObject) //on new object hovered
+            {
+                if (PreviousHitObject != null && PreviousHitObject.GetComponent<UICore>() != null)
+                {
+                    PreviousHitObject.GetComponent<UICore>().OnHoverExit();
+                }
+
+                if (HitObject != null && HitObject.GetComponent<UICore>() != null)
+                {
+                    HitObject.GetComponent<UICore>().OnHoverEnter();
+                }
+                
+                PreviousHitObject = HitObject;
+            }
+
+            if (Input.GetMouseButtonDown(0)) //when you click
+            {
+                if (HitObject.GetComponent<UICore>() == null) return;
+                HitObject.GetComponent<UICore>().OnClickObject();
+            }
+            if (Input.GetMouseButtonUp(0)) //when you lift the mouse back up
+            {
+                if (HitObject.GetComponent<UICore>() == null) return;
+                HitObject.GetComponent<UICore>().OnClickStop();
+            }
+        }
+    }
+}
